caf919779c692454bf77a8436533d8eb9c2d12f4
Moshe Stern <moshecstern@gmail.com>
2019-11-07 19:38:25 -0500

    changed profile pic, added fav projects to main page. added js file

---
+++ portfolio.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function () {
+
+console.log("hello");
+
+// $("#contact").on("click", function(){
+//     // event.preventDefault();
+//     if ("footer" != "50%"){
+//         console.log("iv been clicked");
+//         $("footer").css("height", "50%");
+//     }else {
+//         console.log("clicked again");
+//         $("footer").css("height", "15%");
+//         $("footer").css("background-color", "white");
+//     }
+// })
+
+// $("#contact").on("click", function(){
+//     // event.preventDefault();
+//     if ("footer" == "50%"){
+//         console.log("iv been clicked");
+//         $("footer").css("height", "15%");
+//         $("footer").css("background-color", "white");
+//     }else {
+//         console.log("clicked again");
+//         $("footer").css("height", "50%");
+//     }
+// })
+
+}); // end of js
